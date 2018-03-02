de3bd581023bfe1d4697ffd971e6cd6c9d5a1dfa
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100215-Ronald.local>
2018-03-01 20:59:09 -0500

    Primera clase de sinatra

--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
-
+require "sinatra/reloader" if development?
 get '/' do
-  'Hello world!'
+
+  'Hello !'
+end
+
+get '/:city' do # path variables
+  "esta ciudad #{params[:city]}"
+end
+
+get '/dado/:numero' do
+  num = 4
+  if params[:numero].to_i == num
+    erb :ganaste
+  else
+    "el fbi va por ti"
+ end
 end
